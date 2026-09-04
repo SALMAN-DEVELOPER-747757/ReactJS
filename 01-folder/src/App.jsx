370fb5fc8cdcfe927e448c4acb8b512dfe44b746
SALMAN-DEVELOPER-747757 <salmankhantecwork@gmail.com>
2026-09-05 00:22:03 +0500

    01-folder

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+function App(){
+return (
+<>                                        //this work as Raper
+    <div id='parent'>
+    <h1 id='child 1'>hello friends</h1>
+    <h3 id='child 2'>hello berlin</h3>
+  </div>
+  <div id="chacha"></div>
+  </>
+)
+}
+
+export default App
+// npm create vite we used for creating folder
