d73c49636c7160a00c2f0a0d208d72e2fb84ca1e
SALMAN-DEVELOPER-747757 <salmankhantecwork@gmail.com>
2026-09-05 00:26:08 +0500

    Add 02-components

---
+++ 02-components/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Card from './components/Card'
+import Navbar from './components/Navbar'
+// const user ='Berlin';
+// let age=22
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Navbar />
+   <Card />
+    <Card />
+    </div>
+  )
+
+}
+
+
+export default App
---
+++ 02-components/src/components/Card.jsx
@@ -0,0 +1,8 @@
+function Card (){
+   return <div className="card">
+        <h1>This is me salman khan</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, quam?</p>
+
+    </div>
+}
+export default Card;
